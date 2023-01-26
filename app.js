52bad63312aff6b1de04833d19942925d7d32f36
kwameasannte42 <coolllyod@gmail.com>
2023-01-25 22:28:46 -0500

    first push

---
+++ app.js
@@ -0,0 +1,23 @@
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var numberFour = 4;
+var numberFive = 5;
+var numberSix = 6;
+var numberSeven = 7;
+var numberEight = 8;
+var numberNine = 9;
+var numberTen = 10;
+
+console.log(numberOne + numberTwo);
+
+console.log(numberThree - numberFour);
+
+console.log(numberFive * numberSix);
+
+console.log(numberSeven / numberEight);
+
+console.log(++numberNine);
+
+console.log(--numberTen)
+
